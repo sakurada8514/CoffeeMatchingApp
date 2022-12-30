1938e3f4271e6009dba73ffd825a6c7d9930f790
ryotasakurada <sryota@quatre-re.jp>
2022-12-30 11:47:00 +0900

    tailwind 設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+import "@/styles/globals.css";
+import "@aws-amplify/ui-react/styles.css";
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div data-theme="coffee" className="min-h-screen">
+      <Component {...pageProps} />;
+    </div>
+  );
 }
